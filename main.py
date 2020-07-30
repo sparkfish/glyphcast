85e3b3b1190f9c457e1ebc37485f0b9ed5326420
Hugo Saavedra <hsaavedra@sparkfish.com>
2020-07-30 15:05:50 -0600

    Use modules for conversions instead of inlining conversion logic
    This change also changes the HTTP method from GET to PUT, and requires
    that a querystring specifying the conversion "to" and "from" file
    formats be provided
    
    It also adds logging, and more specific HTTP status codes in the event of different
    kinds of failures

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
-import base64
-from io import BytesIO
+import logging
 
-import cairosvg
+from glyphcast.converters import Converter, UnsupportedConversionException
+from glyphcast.formats import Format
 
 from flask import Flask, request, send_file
 
+logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s', level=logging.DEBUG)
+
 app = Flask(__name__)
 
-
-def svg_to_pdf(svg_text):
-  pdf_buffer = BytesIO()
-  cairosvg.svg2pdf(bytestring=str(svg_text), write_to=pdf_buffer)
-  pdf_buffer.seek(0)
-  return pdf_buffer
-
-@app.route("/", methods=['GET'])
-def convert_svg():
+# TODO: Add rate-limiting, configurable max file size limits
+# TODO: Move the body of this method to a handler module
+@app.route("/", methods=['PUT'])
+def convert():
+    # Determine source/to conversion formats, possibly from filename extension or querystring
+    # Attempt to perform the conversion
+    # If conversion fails, return a 400 if due to unsupported format or malformed data
+    # If conversion fails due to something else, return a 500 error
+    from_ = request.args.get("from", "")
+    to = request.args.get("to", "")
+    file_data = request.data
+    conversion_type = Converter.conversion_type(from_, to)
+    logging.info(f"Received query with conversion type {conversion_type}")
+    converter = Converter(*conversion_type)
     try:
-        svg_data = request.get_data(as_text=True)
-        svg_pdf = svg_to_pdf(svg_data)
-        response = send_file(svg_pdf, attachment_filename='svg.pdf', mimetype='application/pdf')
+        converted = converter.convert(file_data)
+        # TODO: Log the size of the converted file in bytes. This is returned
+        #       when writing to the BytesIO buffer in the underlying conversion
+        #       method. We might return this in future payloads that include file
+        #       metadata
+        logging.info(f"Converted {from_} to {to}")
+        # Only PDF responses are supported currently
+        # When more responses are supported the converter
+        # object can be augmented to determine the correct mimetype to return
+        # Flask.send_file can guess mimetype based on the filename
+        response = send_file(
+          converted,
+          attachment_filename='document.pdf',
+          mimetype='application/pdf',
+          as_attachment=True
+        )
         status = 200
-    except:
-        response = "invalid data"
-        status = 400
+
+    except UnsupportedConversionException as e:
+        status = 415
+        response = e.args[0]
+        logging.warn(f"{response}")
+    except Exception as unhandled:
+        status = 500
+        response = f"Something went wrong on our end"
+        logging.error(f"Encountered an unhandled exception: {unhandled}")
+    logging.info(f"Responding with status code {status}: {response}")
     return response, status
